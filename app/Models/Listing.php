1c226fa0d0f495c182b860005cba056421bbfc90
Joanne Kam <joannekam@Joannes-MacBook-Pro.local>
2022-08-31 13:47:06 -0400

    seeding/factory

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -1,32 +1,11 @@
 <?php
-    namespace App\Models;
 
-    class Listing {
-        public static function all() {
-            return [
-                [
-                'id'=>1,
-                'title' => 'Listing One',
-                'description' => 'sdlkjvnawohvowenvowvnsdicvnjoisedjfcos
-                lsjfowpowefjweknflwenfsdlkjvnawohvowenvowvnsdicvnjoisedjfcos
-                lsjfowpowefjweknflwenf'
-            ],
-            [
-                'id'=>2,
-                'title' => 'Listing two',
-                'description' => 'sdlkjvnawohvowenvowvnsdicvnjoisedjfcos
-                lsjfowpowefjweknflwenfsdlkjvnawohvowenvowvnsdicvnjoisedjfcos
-                lsjfowpowefjweknflwenf'
-            ]
-            ];
-        }
+namespace App\Models;
 
-        public static function find($id){
-            $listings = self::all();
-            foreach($listings as $listing){
-                if($listing['id'] == $id){
-                    return $listing;
-                }
-            }
-        }
-    }
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Listing extends Model
+{
+    use HasFactory;
+}
